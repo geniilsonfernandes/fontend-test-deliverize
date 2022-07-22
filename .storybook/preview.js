444016072ed453c8bf06ffd778ddb0111140df70
Genilson <geniilsonfernandes@gmail.com>
2022-07-22 19:46:09 -0300

    :sparkles:initial: configure initial and initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../src/styles/globalStyles";
+import theme from "../src/styles/theme";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div style={{ margin: " 0 auto", maxWidth: "900px" }}>
+        <Story />
+      </div>
+    </ThemeProvider>
+  )
+];
+
+export const parameters = {
+  backgrounds: {
+    default: "black",
+    values: [
+      { name: "black", value: "#000" },
+      { name: "white", value: "#fff" }
+    ]
+  }
+};
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import "../styles/css/owfont-regular.css";
+
+const GlobalStyle = createGlobalStyle`
+ * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html{
+    font-size: 62.5%;
+  }
+  body{
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.6rem;
+  }
+  input{
+    font-size: 1.6rem;
+    font-family: 'Poppins', sans-serif;
+  }
+  button{
+    font-family: 'Poppins', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
