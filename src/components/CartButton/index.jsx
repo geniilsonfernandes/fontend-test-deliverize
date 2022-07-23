9595c037178690db4f0181bb8fef2e28c0aa5d5b
Genilson <geniilsonfernandes@gmail.com>
2022-07-23 01:21:57 -0300

    :zap:chore(CartButton):add popup in component

--- src/components/CartButton/index.jsx
+++ src/components/CartButton/index.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CartIcon } from "../../Icons";
+import { PopupModel } from "../PopupModel";
+import { ProductInRaw } from "../ProductInRaw";
+
+import P from "prop-types";
 
 import * as S from "./styles";
 
-export const CartButton = () => {
+export const CartButton = ({ counter, product }) => {
+  const [showPopop, setShowPopop] = useState(false);
+
+  useEffect(() => {
+    product && setShowPopop(true);
+    
+    const timer = setTimeout(() => {
+      setShowPopop(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [counter]);
+
   return (
     <S.Wrapper>
       <S.Icon>
-        <S.Ribbon>20</S.Ribbon>
+        <S.Ribbon>{counter}</S.Ribbon>
         <CartIcon />
       </S.Icon>
       Carrinho
+      {showPopop && (
+        <S.PopUp>
+          <PopupModel label="Adicionado com Sucesso" arrowPosition="right">
+            <ProductInRaw />
+          </PopupModel>
+        </S.PopUp>
+      )}
     </S.Wrapper>
   );
+};
+
+CartButton.propTypes = {
+  counter: P.number,
+  product: P.object
 };
