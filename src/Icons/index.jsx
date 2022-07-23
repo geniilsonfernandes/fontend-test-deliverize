49572d8f7da52b2d022e063322f157c9810654ff
Genilson <geniilsonfernandes@gmail.com>
2022-07-23 03:05:18 -0300

    :zap:chore(icons):add logo

--- src/Icons/index.jsx
+++ src/Icons/index.jsx
@@ -1,3 +1,7 @@
 export { ReactComponent as ArrowIcon } from "./arrow.svg";
 export { ReactComponent as UserIcon } from "./user.svg";
 export { ReactComponent as CartIcon } from "./cart.svg";
+export { ReactComponent as Logo } from "./logo.svg";
+
+
+
