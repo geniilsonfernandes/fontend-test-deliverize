7fff0df26c2a43cd66b30c40f75e5a6853b5b282
Genilson <geniilsonfernandes@gmail.com>
2022-07-22 21:15:27 -0300

    :zap:chore(Address): add accessibility tags

--- src/components/Address/index.jsx
+++ src/components/Address/index.jsx
@@ -6,12 +6,12 @@ import * as S from "./styles";
 
 export const Address = ({ currentAddress }) => {
   return (
-    <S.Wrapper tabIndex="1">
+    <S.Wrapper tabIndex="1" aria-label="address" role="button">
       <S.Content>
         <S.Label>Entrega:</S.Label>
         <S.Address> {currentAddress}</S.Address>
       </S.Content>
-      <S.Icon>
+      <S.Icon aria-label="open address">
         <ArrowIcon />
       </S.Icon>
     </S.Wrapper>
