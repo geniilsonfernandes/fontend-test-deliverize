15a0061c89f4418282e22b297f871bb9a1fccc2e
Genilson <geniilsonfernandes@gmail.com>
2022-07-23 14:53:20 -0300

    :zap:chore(icons):add xis icon

--- src/Icons/index.jsx
+++ src/Icons/index.jsx
@@ -2,6 +2,7 @@ export { ReactComponent as ArrowIcon } from "./arrow.svg";
 export { ReactComponent as UserIcon } from "./user.svg";
 export { ReactComponent as CartIcon } from "./cart.svg";
 export { ReactComponent as Logo } from "./logo.svg";
+export { ReactComponent as XisIcon } from "./xis.svg";
 
 
 
