9c920d716b11593c2d4e446a60a2d8faffee179d
Genilson <geniilsonfernandes@gmail.com>
2022-07-23 01:21:08 -0300

    :bug:fix(search):fix warning

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,7 +4,7 @@ import P from "prop-types";
 import * as S from "./styles";
 
 export const Search = ({ onSubmit }) => {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     value && onSubmit(value);
@@ -14,9 +14,9 @@ export const Search = ({ onSubmit }) => {
     <S.Wrapper onSubmit={handleSubmit}>
       <input
         type="text"
+        value={value}
         placeholder="Busque por estabelecimento ou produtos"
         onChange={(e) => setValue(e.target.value)}
-        value={value}
       />
     </S.Wrapper>
   );
