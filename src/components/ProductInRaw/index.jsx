3073628a6cbc087cee95ed3e4733e639a904a214
Genilson <geniilsonfernandes@gmail.com>
2022-07-23 01:21:35 -0300

    :zap:chore(ProductInRaw ):create prduct raw

---
+++ src/components/ProductInRaw/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import * as S from "./styles";
+
+export const ProductInRaw = () => {
+  return (
+    <S.ProductInRaw>
+      <h2>Oferta Cheddar Bacon</h2>
+      <span>Ingredientes:</span>
+      <ul>
+        <li>1 Carne 250gr</li>
+        <li>2 Queijo Cheddar</li>
+        <li>1 Bacon</li>
+        <li>Molho Especial</li>
+      </ul>
+    </S.ProductInRaw>
+  );
+};
---
+++ src/components/ProductInRaw/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const ProductInRaw = styled.div`
+  text-align: initial;
+  padding: 8px;
+  h2 {
+    font-weight: 700;
+    font-size: 1.4rem;
+    color: #bc2e31;
+  }
+  span {
+    font-size: 1.2rem;
+    color: #4e4e4e;
+    display: block;
+    padding-top: 8px;
+    padding-bottom: 8px;
+  }
+  ul {
+    list-style: none;
+    li {
+      font-size: 1.2rem;
+      color: #4e4e4e;
+      ::before {
+        content: "•";
+        padding: 0 8px;
+      }
+      &:not(:last-child) {
+        padding-bottom: 4px;
+      }
+    }
+  }
+`;
