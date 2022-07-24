c71ca8fd27ffd1749ae1003d5d9ed4ad867999f7
Genilson <geniilsonfernandes@gmail.com>
2022-07-24 00:22:22 -0300

    :zap:(theme): add new colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,6 +6,11 @@ export default {
       400: "#ebebeb",
       500: "#E5E5E5"
     },
+    black: {
+      100: "#C0C0C0",
+      200: "#656363",
+      300: "#4E4E4E"
+    },
     red: {
       100: "#D80000"
     },
@@ -15,7 +20,8 @@ export default {
     overlay: "rgba(50, 48, 48, 0.85)",
     gray: {
       light: "#C0C0C0",
-      darker: "#656363"
+      darker: "#656363",
+      extra: "#4E4E4E"
     }
   },
   box_shadow: {
