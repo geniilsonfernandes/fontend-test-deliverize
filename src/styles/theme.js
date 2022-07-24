fd33d4abd30617cc3506f334a3e92f432cbd977d
Genilson <geniilsonfernandes@gmail.com>
2022-07-24 10:45:31 -0300

    :zap:(theme): organize colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -9,27 +9,35 @@ export default {
     black: {
       100: "#C0C0C0",
       200: "#656363",
-      300: "#4E4E4E"
+      300: "#4E4E4E",
+      light: "#C0C0C0",
+      darker: "#656363",
+      extra: "#4E4E4E"
     },
     red: {
       100: "#D80000"
     },
     yallow: {
-      100: "#F09035"
+      100: "#F09035",
+      200: "#F1B074",
+      300: "#F9CF90",
+      400: "#FFF7CF"
     },
-    overlay: "rgba(50, 48, 48, 0.85)",
-    gray: {
-      light: "#C0C0C0",
-      darker: "#656363",
-      extra: "#4E4E4E"
-    }
+    overlay: "rgba(50, 48, 48, 0.85)"
   },
   box_shadow: {
     small: "0px 1px 2px rgba(0, 0, 0, 0.1);",
     huge: "0px 2px 4px rgba(0, 0, 0, 0.15);"
   },
   radius: "4px",
-  fontSizes: {},
+  fontSizes: {
+    xsmall: "1.2rem",
+    small: "1.4rem",
+    medium: "1.6rem",
+    large: "2.6rem",
+    xlarge: "4.2rem",
+    biger: "5.2rem"
+  },
   media: {
     small: "1068px"
   }
