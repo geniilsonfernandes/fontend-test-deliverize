5493563e8405e7106ff98638fe1273787b12019d
Genilson <geniilsonfernandes@gmail.com>
2022-07-23 23:57:36 -0300

    :zap:chore(icons): add plus and none remove icon

--- src/Icons/index.jsx
+++ src/Icons/index.jsx
@@ -4,6 +4,5 @@ export { ReactComponent as CartIcon } from "./cart.svg";
 export { ReactComponent as Logo } from "./logo.svg";
 export { ReactComponent as XisIcon } from "./xis.svg";
 export { ReactComponent as EmptyIcon } from "./empty.svg";
-
-
-
+export { ReactComponent as AddIcon } from "./add.svg";
+export { ReactComponent as RemoveIcon } from "./remove.svg";
