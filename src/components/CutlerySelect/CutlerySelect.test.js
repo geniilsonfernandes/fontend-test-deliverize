91df022ed2654843d619c16140822ab4cdd4557a
Genilson <geniilsonfernandes@gmail.com>
2022-07-24 10:17:27 -0300

    :heavy_check_mark:test(CutlerySelect):add tests

---
+++ src/components/CutlerySelect/CutlerySelect.test.js
@@ -0,0 +1,28 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { CutlerySelect } from ".";
+import renderWithTheme from "../../utils/test/renderWithTheme";
+
+const mockProps = {
+  onSelect: jest.fn()
+};
+
+describe("<IngredientsControl />", () => {
+  it("should render and select sim Button", () => {
+    renderWithTheme(<CutlerySelect {...mockProps} />);
+
+    const buttonYes = screen.getByText("Sim");
+    const buttonNo = screen.getByText("Não");
+
+    expect(buttonYes).toBeInTheDocument();
+    expect(buttonNo).toBeInTheDocument();
+
+    userEvent.click(buttonYes);
+
+    expect(mockProps.onSelect).toBeCalledWith("Sim");
+
+    userEvent.click(buttonNo);
+
+    expect(mockProps.onSelect).toBeCalledWith("Não");
+  });
+});
