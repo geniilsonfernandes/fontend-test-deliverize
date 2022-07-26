14ed6eaeb601aef6e42885a2c087a69597985688
Genilson <geniilsonfernandes@gmail.com>
2022-07-26 19:09:55 -0300

    configure storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,16 +3,19 @@ import { ThemeProvider } from "styled-components";
 import { OrderProvider } from "../src/context/orderContext";
 import GlobalStyles from "../src/styles/globalStyles";
 import theme from "../src/styles/theme";
+import { BrowserRouter } from "react-router-dom";
 
 export const decorators = [
   (Story) => (
     <OrderProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <div style={{ margin: " 0 auto", maxWidth: "1440px" }}>
-          <Story />
-        </div>
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <div style={{ margin: " 0 auto", maxWidth: "1440px" }}>
+            <Story />
+          </div>
+        </ThemeProvider>
+      </BrowserRouter>
     </OrderProvider>
   )
 ];
