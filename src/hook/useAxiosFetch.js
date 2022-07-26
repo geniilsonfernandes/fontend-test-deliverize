d4f73c614dec0a83269af03ad846b99c766f33e0
Genilson <geniilsonfernandes@gmail.com>
2022-07-26 17:42:05 -0300

    :heavy_check_mark:test(): fix test , remove log

--- src/hook/useAxiosFetch.js
+++ src/hook/useAxiosFetch.js
@@ -18,7 +18,6 @@ const useAxiosFetch = (https) => {
         setError(false);
         setIsLoading(false);
       } catch (error) {
-        console.log(error);
         setError(error);
         setIsLoading(false);
       }
